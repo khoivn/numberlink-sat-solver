74fa2ce59921d7b31878c1d01df3f3c60f5b09de
khoivn <vnkhoi76@gmail.com>
2021-11-20 12:22:23 +0700

    change port

--- App.py
+++ App.py
@@ -21,5 +21,5 @@ def solve():
 
 
 if __name__ == "__main__":
-    # serve(app, host="0.0.0.0", port=7777)
-    app.run(host="0.0.0.0", port=7777)
+    serve(app, host="0.0.0.0", port=8888)
+    # app.run(host="0.0.0.0", port=8888)
